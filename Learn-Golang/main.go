cca7ca55f6490d58934930653977e4b7217a1411
Imam Rangkuti <imam.rangkuti@outlook.com>
2023-02-23 19:05:12 +0700

    Auto stash before merge of "Learn-Golang" and "origin/Learn-Golang"

--- Learn-Golang/main.go
+++ Learn-Golang/main.go
@@ -2,8 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
+var x int
 
+func main() {
+	fmt.Println(x)
+	x++
+	fmt.Println(x)
+
+	foo()
+
+	y := incremetor()()
+
+	fmt.Println(y)
 	x := foo(5)
 	n, s := bar(15, "Hai")
 	fmt.Println(x)
@@ -13,8 +23,14 @@ func main() {
 
 func foo(n int) int {
 	return n
+func foo() {
+	fmt.Println(x)
 }
 
 func bar(n int, s string) (int, string) {
 	return n, s
+func incremetor() func() int {
+	return func() int {
+		return 1
+	}
 }
